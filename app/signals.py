4a6f8f8c20f13318481bccbc283633af51c671e0
Vohidjon <vohidjonboyqoziyev@yahoo.com>
2024-08-23 18:53:08 +0500

    signal cache cleared

--- app/signals.py
+++ app/signals.py
@@ -18,6 +18,26 @@ def delete_saved_product(sender, instance, **kwargs):
     cache.delete(f'product_detail_{instance.id}')
 
 
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.core.cache import cache
+
+from .models import Product
+
+@receiver(post_save, sender=Product)
+@receiver(post_delete, sender=Product)
+def clear_product_cache(sender, instance, **kwargs):
+    # Product list keshini tozalash
+    category_slug = instance.group.category.slug if instance.group and instance.group.category else None
+    group_slug = instance.group.slug if instance.group else None
+
+    # Kesh kalitlarini yaratish
+    list_cache_key = f'product_list_{category_slug}_{group_slug}'
+    detail_cache_key = f'product_detail_{instance.slug}'
+
+    # Keshni tozalash
+    cache.delete(list_cache_key)
+    cache.delete(detail_cache_key)
 
 # @receiver(post_save, sender=User)
 # def save_profile(sender, instance, created, **kwargs):
